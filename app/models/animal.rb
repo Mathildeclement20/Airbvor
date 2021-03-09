b11e0e7c986cdd2bf66d696095bc328460ef48f8
yanis khellaf <yanis.khellaf@outlook.fr>
2021-03-09 12:23:19 +0100

    validation de animal ok

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,9 @@
 class Animal < ApplicationRecord
   belongs_to :user
+  SPECIES = %w[lapin vegan escargot lama]
+  validates :specie, inclusion: { in: SPECIES }
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_per_day, presence: true
+  validates :image_url, presence: true
 end
