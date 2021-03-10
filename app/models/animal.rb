e2909f666c037f286b3884650a82928481c46cbb
yanis khellaf <yanis.khellaf@outlook.fr>
2021-03-10 16:33:43 +0100

    help pease

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -8,4 +8,10 @@ class Animal < ApplicationRecord
   validates :description, presence: true
   validates :price_per_day, presence: true
   # validates :image_url, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
+  def self.species
+    return SPECIES
+  end
 end
